2245c547027c6ce4df572c5de06bd9d77a4d4b66
Aneal07 <anilmaharjan07@outlook.com>
2024-08-13 12:07:29 +0100

    customfromfield complete

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -14,6 +14,16 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import CustomFormField from "../../app/CustomFormField";
+export enum FormFieldType {
+  INPUT = "input",
+  TEXTAREA = "textarea",
+  PHONE_INPUT = "phoneInput",
+  CHECKBOX = "checkbox",
+  DATE_PICKER = "datePicker",
+  SELECT = "select",
+  SKELETON = "skeleton",
+}
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -38,7 +48,20 @@ const PatientForm = () => {
   }
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+          <h1 className="header">Hi there 👋</h1>
+          <p className="text-dark-700">Schedule your first appointment.</p>
+        </section>
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="name"
+          label="full name"
+          placeholder="john Doe"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="user"
+        />
         <FormField
           control={form.control}
           name="username"
